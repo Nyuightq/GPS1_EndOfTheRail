a9c8845dad5934cab8a1e341bc0733e02dc44699
ZUKO <elainechew01@gmail.com>
2025-11-18 10:53:05 +0800

    Instructions and Train Orb changes abit

--- Assets/!Sources/Scripts/UI/Gameplay/OnTrainLight.cs
+++ Assets/!Sources/Scripts/UI/Gameplay/OnTrainLight.cs
@@ -94,7 +94,7 @@ public class OnTrainLight : MonoBehaviour
             .SetTarget(trainLight.gameObject);
 
             // --- ORB Tween (your shorter duration for going back to day) ---
-            float orbDuration = isDay ? transitionDuration : 0.15f;
+            float orbDuration = isDay ? transitionDuration : 0.25f;
 
             DOTween.To(
                 () => orbRenderer.color.a,
